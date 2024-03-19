33b56f008a6bc5217936f910613137bc41468ee3
priscilladhandoyo <priscillahandoyo9@gmail.com>
2024-03-19 12:45:13 +1100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -4,8 +4,12 @@ words = ['python', 'java', 'kolin', 'javascript', 'ruby', 'swift']
 
 #randomly choose a word from the list
 chosen_word = random.choice(words)
-word_display = ['_' for _ in chosen_word] #create a list of underscores
-attempts = 8 #number of allowed attempts
+
+#create a list of underscores which equal to the length of chosen_word
+word_display = ['_' for _ in chosen_word]
+
+#number of allowed attempts
+attempts = 8 
 
 print("Welcome to Hangman!")
 
@@ -13,13 +17,20 @@ print("Welcome to Hangman!")
 #until the all the "_" have a alphabets in it or
 #until they run out of attempts
 while attempts > 0 and '_' in word_display:
-    print("\n" + ' '.join(word_display))
+
+    #take an empty string and we concatenate it with every element of the word display
+    print("\n" + ' '.join(word_display)) 
     guess = input("Guess a letter: ").lower()
+
+    #check if the guess letters are in the chosen_word
     if guess in chosen_word:
+
         #for ex: python, index = 0, letter = p
         for index, letter in enumerate(chosen_word):
             if letter == guess:
-                word_display[index] = guess #reveal the letter
+
+                #reveal the letter
+                word_display[index] = guess
     else:
         print("That letter doesn't appear in the word, idiot !!!")
         attempts -= 1
